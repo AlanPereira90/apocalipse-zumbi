5fdb2301df7fc82c0e028c5021e4f156f202ece6
Alan Gabriel Pereira <alangpereira1990@gmail.com>
2019-04-03 10:29:10 -0300

    Adicionado animação, colisão e física à heroína

---
+++ Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour {
+
+	public float Velocidade = 10;
+	Vector3 direcao;
+
+	// Update is called once per frame
+	void Update () {
+
+		float eixoX = Input.GetAxis("Horizontal");
+		float eixoZ = Input.GetAxis("Vertical");
+
+		direcao = new Vector3(eixoX,0,eixoZ);		
+
+		if (direcao != Vector3.zero){
+			GetComponent<Animator>().SetBool("movendo",true);
+		}
+		else{
+			GetComponent<Animator>().SetBool("movendo",false);
+		}
+		
+	}
+
+	void FixedUpdate() {
+
+		GetComponent<Rigidbody>().MovePosition
+		  (GetComponent<Rigidbody>().position + (direcao * Velocidade * Time.deltaTime));
+		
+	}
+}
